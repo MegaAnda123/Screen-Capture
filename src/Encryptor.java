aa1e1fc07cfb04c8a4e60d5f7d00230c9789025a
Zekael <nilssen-m@hotmail.com>
2019-12-31 18:43:16 +0100

    Documentation

--- src/Encryptor.java
+++ src/Encryptor.java
@@ -16,9 +16,9 @@ public class Encryptor {
 
     /**
      * Standard AES Encryption
-     * @param input
-     * @param secret
-     * @return
+     * @param input String you want encrypted
+     * @param secret shared password/secret
+     * @return Encrypted String
      */
     public static String encryptSymmetric(String input, String secret){
 
@@ -39,9 +39,9 @@ public class Encryptor {
 
     /**
      * Standard AES Decryption
-     * @param input
-     * @param secret
-     * @return
+     * @param input String of encrypted message
+     * @param secret secret password used to encrypt message
+     * @return un encrypted String
      */
     public static String decryptSymmetric(String input,String secret){
 
@@ -62,8 +62,8 @@ public class Encryptor {
 
 
     /**
-     * Sets key used for encryption and decryption
-      * @param keyIn
+     * Sets key used for encryption and decryption and take it from a string to a SecretKeySpec stored statically in the class
+      * @param keyIn Wanted Secret
      */
     private static void setKey(String keyIn){
         MessageDigest sha = null;
@@ -80,7 +80,11 @@ public class Encryptor {
         }
     }
 
-
+    /**
+     * Generate a key pari for Public/private key encryption
+     * @return KeyPair
+     * @throws Exception
+     */
     public static KeyPair generateKeyPair() throws Exception {
         KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
         generator.initialize(1024, new SecureRandom());
@@ -89,6 +93,13 @@ public class Encryptor {
         return pair;
     }
 
+    /**
+     * Encrypts plainText with a public key
+     * @param plainText String message that you want encrypted
+     * @param publicKey Public key from KeyPair
+     * @return encrypted String
+     * @throws Exception
+     */
     public static String encryptWithPublicKey(String plainText, PublicKey publicKey) throws Exception {
         Cipher encryptCipher = Cipher.getInstance("RSA");
         encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
@@ -98,6 +109,13 @@ public class Encryptor {
         return Base64.getEncoder().encodeToString(cipherText);
     }
 
+    /**
+     * Decrypt with Private key, turns message encrypted with its public key back into readable string
+     * @param cipherText Encrypted text from public key encryption
+     * @param privateKey Private key from key pair
+     * @return  Decrypted string
+     * @throws Exception
+     */
     public static String decryptWithPrivateKey(String cipherText, PrivateKey privateKey) throws Exception {
         byte[] bytes = Base64.getDecoder().decode(cipherText);
 
