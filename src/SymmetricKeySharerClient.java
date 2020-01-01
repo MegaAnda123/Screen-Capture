f70608de48eb513ca8687c4601981440374cb438
Zekael <nilssen-m@hotmail.com>
2020-01-01 18:09:39 +0100

    Client and ServerSide protocols for Symmetric Key sharing

--- src/SymmetricKeySharerClient.java
+++ src/SymmetricKeySharerClient.java
@@ -1,23 +1,59 @@
 import javax.crypto.SecretKey;
 import java.io.*;
 import java.net.Socket;
+import java.security.PublicKey;
 
 public class SymmetricKeySharerClient {
 
-    private static String startSymShareCommand = "startSymSharer";
-
-    public static boolean clientSideProtocol(Socket socket){
+    /**
+     * Generic secret key sharer for AES encryption and public/private Key to get to key with server
+     * Client side protocol
+     * @param socket Socket used to connect to server
+     * @return SecretKey used for symmetric AES encryption
+     */
+    public static SecretKey clientSideProtocol(Socket socket){
         try {
+            //Writers and readers
             PrintWriter pw = new PrintWriter(socket.getOutputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+            BufferedReader br = new BufferedReader(isr);
 
-            pw.println(startSymShareCommand);
+            //Generate SecretKey
+            SecretKey symmetricKey = Encryptor.generateSymmetricKey();
 
-            
+            //Serialize SecretKey
+            String serializedKey = Serializer.ObjectToString(symmetricKey);
 
-        } catch (IOException e) {
+            //Receive public key
+            String keyString = br.readLine();
+
+            //Deserialize
+            PublicKey publicKey = (PublicKey) Serializer.ObjectFromString(keyString);
+
+            //Encrypt secretKey
+            String encryptedSerializeKey = Encryptor.publicKeyEncryptor(serializedKey,publicKey);
+
+            //Send Encrypted Key to server
+            pw.println(encryptedSerializeKey);
+
+            //receive confirm
+            String conformation = br.readLine();
+            System.out.println("Conformation: "+ conformation);
+
+            //Check confirm and return key
+            if(conformation.equals("200")){
+                //return key
+                return symmetricKey;
+            }
+
+        } catch (ClassNotFoundException | IOException e) {
             e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Unexpected Error");
         }
+        return null;
     }
 
 }
